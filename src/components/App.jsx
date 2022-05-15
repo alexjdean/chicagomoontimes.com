271c1dc1f35f6215dae6f76b0a388671a6acd5ff
Alexander Dean <alexjacobdean1@gmail.com>
2022-05-14 21:23:00 -0500

    Add background, spacing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,10 @@ import Navbar from "./Navbar"
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages';
 import About from './pages/about';
+import Archives from './pages/archive';
+import Donate from './pages/donate'
+import Subscribe from './pages/subscribe'
+import Footer from './Footer'
 
 function App() {
     return <div>
@@ -11,7 +15,11 @@ function App() {
         <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/about' element={<About />} />
+            <Route path='/archives' element={<Archives />} />
+            <Route path='/donate' element={<Donate />} />
+            <Route path='/subscribe' element={<Subscribe />} />
         </Routes>
+        <Footer />
     </Router>
     </div>;
 }
--- src/components/pages/subscribe.jsx
+++ src/components/pages/subscribe.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const About = () => {
+const Subscribe = () => {
   return (
     <div
       style={{
@@ -11,9 +11,9 @@ const About = () => {
         color: '#fff'
       }}
     >
-      <h1>About</h1>
+      <h1>Subscribe</h1>
     </div>
   );
 };
 
-export default About;
+export default Subscribe;
