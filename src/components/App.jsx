22c329ee0ca92b2889ab509d60ce106a940f8735
Alexander Dean <alexjacobdean1@gmail.com>
2022-05-18 12:11:25 -0500

    More work

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,8 +6,25 @@ import About from './pages/about';
 import Donate from './pages/donate'
 import Subscribe from './pages/subscribe'
 import Footer from './Footer'
+import Article from "./pages/article"
+import Info from "./info"
+
+function createArticleRoutes(article) {
+    var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    var articleDate = new Date(article.date);
+
+    return (<Route path={"/" + article.path} 
+        element=<Article
+            title={article.title}
+            date={months[articleDate.getMonth()] + ' ' + articleDate.getDate() + ', ' + articleDate.getFullYear()}
+            image={article.image}
+            content={article.content}
+        />/>);
+}
 
 function App() {
+    Info.sort(function(x, y){ return x.spotlight === true ? -1 : y.spotlight === true ? 1 : 0;});
+    
     return <div>
     <Router>
         <Navbar />
@@ -16,6 +33,7 @@ function App() {
             <Route path='/about' element={<About />} />
             <Route path='/donate' element={<Donate />} />
             <Route path='/subscribe' element={<Subscribe />} />
+            {Info.map(createArticleRoutes)}
         </Routes>
         <Footer />
     </Router>
