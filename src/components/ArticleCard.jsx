09a97769ab07d49740c32b7710c7f3d0c827f1ab
Alexander Dean <alexjacobdean1@gmail.com>
2022-05-21 13:23:19 -0500

    Add more features

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import { useMediaQuery } from 'react-responsive'
 
 function ArticleCard(props) {
     let page_style = {};
-    let title_style = {}
+    let title_style = {};
+    const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1224px)' })
 
     if (props.spotlight === true) {
         page_style = {
@@ -29,13 +31,31 @@ function ArticleCard(props) {
             maxWidth: "35vh",
             float: "left"
         };
-
-        title_style = {
-            fontSize: "1.25rem",
-            padding: "0.5rem 0",
-            maxHeight: "100%",
-            maxWidth: "100%"
-        };
+        
+        if(isTabletOrMobile) {
+            page_style = {
+                padding: "20px 15px",
+                margin: "10px 25px",
+                display: "flex",
+                flexDirection: "column",
+                maxWidth: "80vh",
+                float: "left"
+            };
+            
+            title_style = {
+                fontSize: "1.75rem",
+                padding: "0.5rem 0",
+                maxHeight: "100%",
+                maxWidth: "100%"
+            };
+        } else {
+            title_style = {
+                fontSize: "1.25rem",
+                padding: "0.5rem 0",
+                maxHeight: "100%",
+                maxWidth: "100%"
+            };
+        }
     }  
 
     return (<div style={page_style}>
@@ -45,7 +65,7 @@ function ArticleCard(props) {
             maxWidth: "100%"
         }} src={props.image} alt="article_image"/>
 
-        <h1 className="articleTitle" style={title_style}>{props.title}</h1>
+        <h1 className="article-title" style={title_style}>{props.title}</h1>
 
         <p style={{
             color: "#DDDDDD",
